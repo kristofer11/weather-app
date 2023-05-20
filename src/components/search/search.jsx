6813fcf0c5e780cd64d3560013b001e79e0e52bb
Kris Hvattum <klhvattum@live.com>
2023-05-19 17:36:22 -0700

    Built 'loadOptions function to set options for AsyncPaginate.

--- src/components/search/search.jsx
+++ src/components/search/search.jsx
@@ -9,8 +9,16 @@ const Search = ({ onSearchChange }) => {
     const loadOptions = async (inputValue) => {
         try {
             const response = await fetch(`${GEO_API_URL}minPopulation=100000&namePrefix=${inputValue}`, geoApiOptions);
-            const result = await response.text();
+            const result = await response.json();
             console.log(result);
+            return {
+                options: result.data.map((city) => {
+                    return {
+                        label: `${city.name}, ${city.countryCode}`,
+                        value: `${city.latitude} ${city.longitude}`
+                    }
+                })
+            }
         } catch (error) {
             console.error(error);
         }
